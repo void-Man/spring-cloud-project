84ff21c4b7ecc672468607b8e065d22075e00988
mengjie_chen <1737756104@qq.com>
2021-07-11 23:11:03 +0800

    commit

---
+++ cloud-provider/provider-payment-8001/src/main/java/com/cmj/example/PamentApplication8001.java
@@ -0,0 +1,16 @@
+package com.cmj.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengjie_chen
+ * @description
+ * @date 2021/7/11
+ */
+@SpringBootApplication
+public class PamentApplication8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PamentApplication8001.class, args);
+    }
+}
