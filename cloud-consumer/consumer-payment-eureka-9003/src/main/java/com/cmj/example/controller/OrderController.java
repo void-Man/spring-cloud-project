10eb16e4155346a0e5503037c0a3783a7e3583a0
mengjie_chen <1737756104@qq.com>
2021-07-18 20:17:01 +0800

    consumer-payment-eureka-9003

---
+++ cloud-consumer/consumer-payment-eureka-9003/src/main/java/com/cmj/example/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.cmj.example.controller;
+
+import com.cmj.example.vo.CommonResultVo;
+import com.cmj.example.vo.PaymentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author mengjie_chen
+ * @description
+ * @date 2021/7/14
+ */
+@RestController
+public class OrderController {
+
+    public static final String PaymentSrv_URL = "http://localhost:8003";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/eureka/payment/create")
+    public CommonResultVo<?> create(@RequestBody PaymentDto paymentDto) {
+        return restTemplate.postForObject(PaymentSrv_URL + "/eureka/payment/create", paymentDto, CommonResultVo.class);
+    }
+
+}
